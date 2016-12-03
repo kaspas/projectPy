cc1a68a7a1e130f94ad52695d4265ded7f07e371
milts10 <miltossaratzidis1@gmail.com>
2016-12-03 14:16:21 +0000

    Version 2 Evaluation function

---
+++ Evaluation.py
@@ -0,0 +1,23 @@
+def Evaluation(Word1,Word2):
+	if len(Word1)<len(Word2):
+		mini=len(Word1)
+		maxi=len(Word2)
+	else:
+		mini=len(Word2)
+		maxi=len(Word1)
+
+	SameValue=0 # counter to poso idia eiani ta 2 string 
+	for i in range(0,mini):
+		if Word1[i]==Word2[i]:
+			SameValue=SameValue+1
+
+	SameValue=float(SameValue)
+	EvalPersent=(SameValue/maxi)*100
+	
+
+	print SameValue
+	print EvalPersent
+	print Word2[0]
+
+Evaluation('miltos','miltiadis')
+
